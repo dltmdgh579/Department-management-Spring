39ff2498983ac84cdab69420ebea4f7d7570c6e4
이승호 <dltmdgh579@gmail.com>
2023-11-16 22:34:01 +0900

    init: 스프링 부트 초기 세팅
    Signed-off-by: 이승호 <dltmdgh579@gmail.com>

---
+++ src/main/java/ministryofeducation/sideprojectspring/SideprojectSpringApplication.java
@@ -0,0 +1,13 @@
+package ministryofeducation.sideprojectspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SideprojectSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SideprojectSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ministryofeducation/sideprojectspring/SideprojectSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package ministryofeducation.sideprojectspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SideprojectSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

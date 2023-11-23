a997c1584f38f33516c17b3150b5500abf918e37
dltmdgh579 <dltmdgh579@naver.com>
2023-11-23 17:23:34 +0900

    refactor: PersonnelListResponse -> PersonnelListDto 이름 변경
    Signed-off-by: dltmdgh579 <dltmdgh579@naver.com>

--- src/main/java/ministryofeducation/sideprojectspring/personnel_list/presentation/PersonnelListController.java
+++ src/main/java/ministryofeducation/sideprojectspring/personnel_list/presentation/PersonnelListController.java
@@ -3,7 +3,7 @@ package ministryofeducation.sideprojectspring.personnel_list.presentation;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import ministryofeducation.sideprojectspring.personnel_list.application.PersonnelListService;
-import ministryofeducation.sideprojectspring.personnel_list.presentation.dto.response.PersonnelListResponse;
+import ministryofeducation.sideprojectspring.personnel_list.presentation.dto.response.PersonnelListDto;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,8 +15,8 @@ public class PersonnelListController {
     private final PersonnelListService personnelListService;
 
     @GetMapping("/list")
-    public ResponseEntity<List<PersonnelListResponse>> personnelList(){
-        List<PersonnelListResponse> responseDto = personnelListService.personnelList();
+    public ResponseEntity<List<PersonnelListDto>> personnelList(){
+        List<PersonnelListDto> responseDto = personnelListService.personnelList();
 
         return ResponseEntity.ok(responseDto);
     }

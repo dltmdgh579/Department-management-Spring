c89e421cb5e633860d8703b8f3475a622ea9d892
dltmdgh579 <dltmdgh579@naver.com>
2023-11-23 16:22:41 +0900

    test: test profile h2 데이터베이스 적용
    Signed-off-by: dltmdgh579 <dltmdgh579@naver.com>

--- src/test/java/ministryofeducation/sideprojectspring/Personnel_list/application/PersonnelListServiceTest.java
+++ src/test/java/ministryofeducation/sideprojectspring/Personnel_list/application/PersonnelListServiceTest.java
@@ -14,9 +14,11 @@ import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 @SpringBootTest
+@ActiveProfiles("test")
 class PersonnelListServiceTest {
 
     @Autowired

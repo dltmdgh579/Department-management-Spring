46e703100a3ed45cc8e319a1cd913f4ac8b334d2
dltmdgh579 <dltmdgh579@naver.com>
2023-11-23 17:24:07 +0900

    feat: response dto 값 추가
    Signed-off-by: dltmdgh579 <dltmdgh579@naver.com>

--- src/main/java/ministryofeducation/sideprojectspring/personnel_list/presentation/dto/response/PersonnelListDto.java
+++ src/main/java/ministryofeducation/sideprojectspring/personnel_list/presentation/dto/response/PersonnelListDto.java
@@ -2,17 +2,35 @@ package ministryofeducation.sideprojectspring.personnel_list.presentation.dto.re
 
 import lombok.Builder;
 import lombok.Getter;
+import ministryofeducation.sideprojectspring.personnel_list.domain.department.Department;
+
+import java.time.LocalDate;
 
 @Getter
 @Builder
-public class PersonnelListResponse {
+public class PersonnelListDto {
 
     private String name;
+    private LocalDate dateOfBirth;
+    private String phone;
+    private String landline;
+    private String email;
+    private String address;
+    private String profileImage;
+    private Department department;
 
-    private PersonnelListResponse(){}
+    private PersonnelListDto(){}
 
-    public PersonnelListResponse(String name){
+    public PersonnelListDto(String name, LocalDate dateOfBirth, String phone, String landline, String email,
+                            String address, String profileImage, Department department){
         this.name = name;
+        this.dateOfBirth = dateOfBirth;
+        this.phone = phone;
+        this.landline = landline;
+        this.email = email;
+        this.address = address;
+        this.profileImage = profileImage;
+        this.department = department;
     }
 
 }

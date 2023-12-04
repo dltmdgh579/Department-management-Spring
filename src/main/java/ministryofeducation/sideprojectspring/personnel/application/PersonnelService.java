5de0e2420996920780579a7a5ccf1649cf5a01ae
이승호 <dltmdgh579@gmail.com>
2023-12-04 21:01:00 +0900

    chore: PersonnelListResponse data 변경 -> landline, email 삭제
    Signed-off-by: 이승호 <dltmdgh579@gmail.com>

--- src/main/java/ministryofeducation/sideprojectspring/personnel/application/PersonnelService.java
+++ src/main/java/ministryofeducation/sideprojectspring/personnel/application/PersonnelService.java
@@ -5,7 +5,7 @@ import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import ministryofeducation.sideprojectspring.personnel.domain.Personnel;
 import ministryofeducation.sideprojectspring.personnel.infrastructure.PersonnelRepository;
-import ministryofeducation.sideprojectspring.personnel.presentation.dto.response.PersonnelListDto;
+import ministryofeducation.sideprojectspring.personnel.presentation.dto.response.PersonnelListResponse;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,22 +14,20 @@ public class PersonnelService {
 
     private final PersonnelRepository personnelRepository;
 
-    public List<PersonnelListDto> personnelList(){
-        List<PersonnelListDto> personnelListResponse = personnelRepository.findAll().stream()
+    public List<PersonnelListResponse> personnelList(){
+        List<PersonnelListResponse> personnelListResponse = personnelRepository.findAll().stream()
             .map(this::getPersonnelListDto)
             .collect(Collectors.toList());
 
         return personnelListResponse;
     }
 
-    public PersonnelListDto getPersonnelListDto(Personnel personnel){
-        return PersonnelListDto.builder()
+    public PersonnelListResponse getPersonnelListDto(Personnel personnel){
+        return PersonnelListResponse.builder()
                 .id(personnel.getId())
                 .name(personnel.getName())
                 .dateOfBirth(personnel.getDateOfBirth())
                 .phone(personnel.getPhone())
-                .landline(personnel.getLandline())
-                .email(personnel.getEmail())
                 .address(personnel.getAddress())
                 .profileImage(personnel.getProfileImage())
                 .department(personnel.getDepartment())
--- src/main/java/ministryofeducation/sideprojectspring/personnel/presentation/dto/response/PersonnelListResponse.java
+++ src/main/java/ministryofeducation/sideprojectspring/personnel/presentation/dto/response/PersonnelListResponse.java
@@ -14,22 +14,18 @@ public class PersonnelListResponse {
     private String name;
     private LocalDate dateOfBirth;
     private String phone;
-    private String landline;
-    private String email;
     private String address;
     private String profileImage;
     private Department department;
 
     private PersonnelListResponse(){}
 
-    public PersonnelListResponse(Long id, String name, LocalDate dateOfBirth, String phone, String landline, String email,
+    public PersonnelListResponse(Long id, String name, LocalDate dateOfBirth, String phone,
                             String address, String profileImage, Department department){
         this.id = id;
         this.name = name;
         this.dateOfBirth = dateOfBirth;
         this.phone = phone;
-        this.landline = landline;
-        this.email = email;
         this.address = address;
         this.profileImage = profileImage;
         this.department = department;

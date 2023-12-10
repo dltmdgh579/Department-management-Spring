9da9e5189721909ac4fc11ea34a65cdacb70323c
이승호 <dltmdgh579@gmail.com>
2023-12-10 21:18:04 +0900

    feat: 부서 정보 조회 domain 구현 (#10)
    Signed-off-by: 이승호 <dltmdgh579@gmail.com>

---
+++ src/main/java/ministryofeducation/sideprojectspring/department/domain/SmallGroup.java
@@ -0,0 +1,33 @@
+package ministryofeducation.sideprojectspring.department.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class SmallGroup {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String leader;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "department_id")
+    private Department department;
+
+
+
+}

dcca0ab30c9546c5fc2472c3fa29711299e0847d
이승호 <dltmdgh579@gmail.com>
2023-12-10 21:22:52 +0900

    feat: 출석기록 domain 추가 (#10)
    Signed-off-by: 이승호 <dltmdgh579@gmail.com>

---
+++ src/main/java/ministryofeducation/sideprojectspring/personnel/domain/Attendance.java
@@ -0,0 +1,35 @@
+package ministryofeducation.sideprojectspring.personnel.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.time.LocalDate;
+import lombok.Getter;
+import ministryofeducation.sideprojectspring.department.domain.Department;
+import ministryofeducation.sideprojectspring.personnel.domain.attendance.AttendanceCheck;
+
+@Entity
+public class Attendance {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private LocalDate attendanceDate;
+    @Enumerated(EnumType.STRING)
+    private AttendanceCheck attendanceCheck;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "department_id")
+    private Department department;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "personnel_id")
+    private Personnel personnel;
+
+}

a18156c6d673a70285f367e2d96ca0412fd9ba2e
이승호 <dltmdgh579@gmail.com>
2023-12-13 19:08:55 +0900

    refactor: 테스트 코드 리팩토링
    Signed-off-by: 이승호 <dltmdgh579@gmail.com>

--- src/test/java/ministryofeducation/sideprojectspring/unit/Personnel/application/PersonnelServiceTest.java
+++ src/test/java/ministryofeducation/sideprojectspring/unit/Personnel/application/PersonnelServiceTest.java
@@ -2,6 +2,7 @@ package ministryofeducation.sideprojectspring.unit.Personnel.application;
 
 import static ministryofeducation.sideprojectspring.factory.PersonnelFactory.*;
 import static org.assertj.core.api.Assertions.*;
+import static org.mockito.BDDMockito.*;
 import static org.mockito.Mockito.*;
 
 import java.util.List;
@@ -16,6 +17,7 @@ import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -33,28 +35,43 @@ class PersonnelServiceTest {
     private PersonnelService personnelService;
 
     @Test
-    public void 전체_인원을_조회할_수_있다() {
+    public void 전체_인원을_조회한다() {
         //given
-        doReturn(testPersonnelList()).when(personnelRepository).findAll();
+        Personnel personnel1 = testPersonnel(1l, "test1", "test1@email.com", "010-0000-0001");
+        Personnel personnel2 = testPersonnel(2l, "test2", "test2@email.com", "010-0000-0002");
+        Personnel personnel3 = testPersonnel(3l, "test3", "test3@email.com", "010-0000-0003");
+        List<Personnel> personnelList = List.of(personnel1, personnel2, personnel3);
+
+        given(personnelRepository.findAll()).willReturn(personnelList);
 
         //when
-        List<PersonnelListResponse> personnelList = personnelService.personnelList();
+        List<PersonnelListResponse> personnelListResponse = personnelService.personnelList();
 
         //then
-        assertThat(personnelList.size()).isEqualTo(3);
+        assertThat(personnelListResponse).hasSize(3)
+            .extracting("name", "phone")
+            .containsExactlyInAnyOrder(
+                tuple("test1", "010-0000-0001"),
+                tuple("test2", "010-0000-0002"),
+                tuple("test3", "010-0000-0003")
+            );
+
+        verify(personnelRepository, times(1)).findAll();
     }
 
     @Test
-    public void Personnel_id로_인원_상세정보를_조회할_수_있다(){
+    public void 인원_상세정보를_조회할_수_있다(){
         //given
-        Personnel testPersonnel = testPersonnel("test1");
-        doReturn(Optional.of(testPersonnel)).when(personnelRepository).findById(1l);
+        Personnel testPersonnel = testPersonnel(1l, "test1");
+        given(personnelRepository.findById(anyLong())).willReturn(Optional.of(testPersonnel));
 
         //when
-        PersonnelDetailResponse personnelDetail = personnelService.personnelDetail(1l);
+        PersonnelDetailResponse personnelDetailResponse = personnelService.personnelDetail(testPersonnel.getId());
 
         //then
-        assertThat(personnelDetail.getName()).isEqualTo("test1");
+        assertThat(personnelDetailResponse.getName()).isEqualTo("test1");
+
+        verify(personnelRepository, times(1)).findById(anyLong());
     }
 
 }

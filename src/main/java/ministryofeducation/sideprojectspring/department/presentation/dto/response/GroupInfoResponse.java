2b42a3b87ef81875b0b03bd82626d4d3570ceb71
이승호 <dltmdgh579@gmail.com>
2023-12-16 18:16:47 +0900

    add: 부서 내 그룹 인원 리스트 Response 값 추가
    Signed-off-by: 이승호 <dltmdgh579@gmail.com>

--- src/main/java/ministryofeducation/sideprojectspring/department/presentation/dto/response/GroupInfoResponse.java
+++ src/main/java/ministryofeducation/sideprojectspring/department/presentation/dto/response/GroupInfoResponse.java
@@ -1,5 +1,6 @@
 package ministryofeducation.sideprojectspring.department.presentation.dto.response;
 
+import java.time.LocalDate;
 import java.util.List;
 import lombok.Builder;
 import lombok.Getter;
@@ -11,12 +12,18 @@ public class GroupInfoResponse {
     private Long id;
     private String profileImage;
     private String name;
+    private LocalDate dateOfBirth;
+    private String phone;
+    private String address;
 
     @Builder
-    private GroupInfoResponse(Long id, String profileImage, String name){
+    private GroupInfoResponse(Long id, String profileImage, String name, LocalDate dateOfBirth, String phone, String address){
         this.id = id;
         this.profileImage = profileImage;
         this.name = name;
+        this.dateOfBirth = dateOfBirth;
+        this.phone = phone;
+        this.address = address;
     }
 
     public static GroupInfoResponse of(Personnel personnel){
@@ -24,6 +31,9 @@ public class GroupInfoResponse {
             .id(personnel.getId())
             .profileImage(personnel.getProfileImage())
             .name(personnel.getName())
+            .dateOfBirth(personnel.getDateOfBirth())
+            .phone(personnel.getPhone())
+            .address(personnel.getAddress())
             .build();
     }
 }

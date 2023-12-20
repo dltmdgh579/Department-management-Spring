f93a2e77ac4aee1079e819352e78319f6b3a1f2d
이승호 <dltmdgh579@gmail.com>
2023-12-20 23:28:18 +0900

    feat: 결석 인원 관리 repository jpql 작성 (#20)
    Signed-off-by: 이승호 <dltmdgh579@gmail.com>

--- src/main/java/ministryofeducation/sideprojectspring/personnel/domain/Attendance.java
+++ src/main/java/ministryofeducation/sideprojectspring/personnel/domain/Attendance.java
@@ -1,5 +1,7 @@
 package ministryofeducation.sideprojectspring.personnel.domain;
 
+import static lombok.AccessLevel.PROTECTED;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
@@ -10,11 +12,14 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import java.time.LocalDate;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import ministryofeducation.sideprojectspring.common.BaseEntity;
 import ministryofeducation.sideprojectspring.department.domain.Department;
 import ministryofeducation.sideprojectspring.personnel.domain.attendance.AttendanceCheck;
 
+@NoArgsConstructor(access = PROTECTED)
 @Entity
 public class Attendance extends BaseEntity {
 
@@ -33,4 +38,24 @@ public class Attendance extends BaseEntity {
     @JoinColumn(name = "personnel_id")
     private Personnel personnel;
 
+    @Builder
+    private Attendance(Long id, LocalDate attendanceDate, AttendanceCheck attendanceCheck, Department department,
+        Personnel personnel) {
+        this.id = id;
+        this.attendanceDate = attendanceDate;
+        this.attendanceCheck = attendanceCheck;
+        this.department = department;
+        this.personnel = personnel;
+    }
+
+    public static Attendance createAttendance(Long id, LocalDate attendanceDate, AttendanceCheck attendanceCheck, Department department,
+        Personnel personnel) {
+        return Attendance.builder()
+            .id(id)
+            .attendanceDate(attendanceDate)
+            .attendanceCheck(attendanceCheck)
+            .department(department)
+            .personnel(personnel)
+            .build();
+    }
 }

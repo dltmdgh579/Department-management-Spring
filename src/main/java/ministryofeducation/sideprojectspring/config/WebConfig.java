b0188a6812c175c3314793c79085adf46d92e7b7
이승호 <dltmdgh579@gmail.com>
2024-01-05 18:32:39 +0900

    fix: profile-image url 경로 수정 -> /api 삭제 (#31)
    Signed-off-by: 이승호 <dltmdgh579@gmail.com>

--- src/main/java/ministryofeducation/sideprojectspring/config/WebConfig.java
+++ src/main/java/ministryofeducation/sideprojectspring/config/WebConfig.java
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     private String resourcePath = "/var/images/personnel/";
-    private String uploadPath = "/api/profile-image/**";
+    private String uploadPath = "/profile-image/**";
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry){

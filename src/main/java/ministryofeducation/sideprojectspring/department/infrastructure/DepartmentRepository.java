e821139ccba7c7ba8c2d56dafb6237783010b5ee
이승호 <dltmdgh579@gmail.com>
2024-01-06 16:11:17 +0900

    feat: personnel add -> department enrollment increase method add
    Signed-off-by: 이승호 <dltmdgh579@gmail.com>

--- src/main/java/ministryofeducation/sideprojectspring/department/infrastructure/DepartmentRepository.java
+++ src/main/java/ministryofeducation/sideprojectspring/department/infrastructure/DepartmentRepository.java
@@ -1,7 +1,10 @@
 package ministryofeducation.sideprojectspring.department.infrastructure;
 
+import java.util.Optional;
 import ministryofeducation.sideprojectspring.department.domain.Department;
+import ministryofeducation.sideprojectspring.personnel.domain.department_type.DepartmentType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DepartmentRepository extends JpaRepository<Department, Long> {
+    Optional<Department> findByDepartmentType(DepartmentType departmentType);
 }

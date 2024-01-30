ba1241e6391807d0db066c1a788ba2d5dd59565c
이승호 <dltmdgh579@gmail.com>
2024-01-30 12:11:46 +0900

    feat: 가장 최근 출석기록 조회 jpa 작성 (#33)
    Signed-off-by: 이승호 <dltmdgh579@gmail.com>

--- src/main/java/ministryofeducation/sideprojectspring/personnel/infrastructure/AttendanceRepository.java
+++ src/main/java/ministryofeducation/sideprojectspring/personnel/infrastructure/AttendanceRepository.java
@@ -1,9 +1,11 @@
 package ministryofeducation.sideprojectspring.personnel.infrastructure;
 
 import java.time.LocalDate;
+import java.util.Optional;
 import ministryofeducation.sideprojectspring.personnel.domain.Attendance;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AttendanceRepository extends JpaRepository<Attendance, Long> {
     Long countByAttendanceDateAndDepartmentId(LocalDate attendanceDate, Long departmentId);
+    Optional<Attendance> findTop1ByPersonnelIdOrderByAttendanceDateDesc(Long personnelId);
 }

310bcef942e9a6386651c72d4d3ca8e1d5259f17
이승호 <dltmdgh579@gmail.com>
2024-01-31 17:14:29 +0900

    add: 트랜잭션 전파 설정 NOT_SUPPORTED
    Signed-off-by: 이승호 <dltmdgh579@gmail.com>

--- src/test/java/ministryofeducation/sideprojectspring/unit/Personnel/infrastructure/AttendanceRepositoryTest.java
+++ src/test/java/ministryofeducation/sideprojectspring/unit/Personnel/infrastructure/AttendanceRepositoryTest.java
@@ -19,8 +19,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 @DataJpaTest
+@Transactional(propagation = Propagation.NOT_SUPPORTED)
 @ActiveProfiles("test")
 class AttendanceRepositoryTest {
 

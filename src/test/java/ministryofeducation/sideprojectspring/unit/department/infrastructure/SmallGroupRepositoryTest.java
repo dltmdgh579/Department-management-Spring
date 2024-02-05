d05a46a30fe70258e8bed17c9253d789a48d2956
이승호 <dltmdgh579@gmail.com>
2024-02-05 17:00:18 +0900

    refactor: create -> builder 변경
    Signed-off-by: 이승호 <dltmdgh579@gmail.com>

--- src/test/java/ministryofeducation/sideprojectspring/unit/department/infrastructure/SmallGroupRepositoryTest.java
+++ src/test/java/ministryofeducation/sideprojectspring/unit/department/infrastructure/SmallGroupRepositoryTest.java
@@ -1,7 +1,6 @@
 package ministryofeducation.sideprojectspring.unit.department.infrastructure;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.List;
 import ministryofeducation.sideprojectspring.config.TestQueryDslConfig;
@@ -9,7 +8,6 @@ import ministryofeducation.sideprojectspring.department.domain.Department;
 import ministryofeducation.sideprojectspring.department.domain.SmallGroup;
 import ministryofeducation.sideprojectspring.department.infrastructure.DepartmentRepository;
 import ministryofeducation.sideprojectspring.department.infrastructure.SmallGroupRepository;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
@@ -33,16 +31,19 @@ class SmallGroupRepositoryTest {
     @Test
     void 부서_Id로_그룹_리스트를_불러온다(){
         //given
-        Department requestDepartment = Department.createDepartment(1l, "department1", 20);
-        departmentRepository.save(requestDepartment);
+        Department department = Department.builder()
+            .name("department")
+            .build();
+        departmentRepository.save(department);
 
-        SmallGroup smallGroup1 = SmallGroup.createSmallGroup(1l, "smallGroup1", "leader1", requestDepartment);
-        SmallGroup smallGroup2 = SmallGroup.createSmallGroup(2l, "smallGroup2", "leader2", requestDepartment);
-        SmallGroup smallGroup3 = SmallGroup.createSmallGroup(3l, "smallGroup3", "leader3", requestDepartment);
+        SmallGroup smallGroup1 = buildSmallGroup("smallGroup1", "leader1", department);
+        SmallGroup smallGroup2 = buildSmallGroup("smallGroup2", "leader2", department);
+        SmallGroup smallGroup3 = buildSmallGroup("smallGroup3", "leader3", department);
+
         smallGroupRepository.saveAll(List.of(smallGroup1, smallGroup2, smallGroup3));
 
         //when
-        List<SmallGroup> smallGroupResponse = smallGroupRepository.findByDepartmentId(requestDepartment.getId());
+        List<SmallGroup> smallGroupResponse = smallGroupRepository.findByDepartmentId(department.getId());
 
         //then
         assertThat(smallGroupResponse).hasSize(3)
@@ -52,5 +53,13 @@ class SmallGroupRepositoryTest {
                 tuple("smallGroup2", "leader2"),
                 tuple("smallGroup3", "leader3")
             );
+    }
+
+    private SmallGroup buildSmallGroup(String smallGroup, String leader, Department department) {
+        return SmallGroup.builder()
+            .name(smallGroup)
+            .leader(leader)
+            .department(department)
+            .build();
     }
 }

9e9c3c2eef5ce1a977fc96c34a6564a730c7349d
이승호 <dltmdgh579@gmail.com>
2024-02-10 23:53:12 +0900

    refactor: 언더바 (_) 삭제
    Signed-off-by: 이승호 <dltmdgh579@gmail.com>

--- src/main/java/ministryofeducation/sideprojectspring/personnel/domain/department_type/DepartmentType.java
+++ src/main/java/ministryofeducation/sideprojectspring/personnel/domain/department_type/DepartmentType.java
@@ -5,8 +5,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum DepartmentType {
     KINDERGARTEN("영유치부"),
-    HOLY_KIDS("초등부"),
-    PAUL_COMMUNITY("청소년부"),
+    HOLYKIDS("초등부"),
+    PAULCOMMUNITY("청소년부"),
     JOSHUA("청년부");
 
     private final String text;

44a18cc5c5bb70693bbae566f7f17dcb2b763b0c
이승호 <dltmdgh579@gmail.com>
2024-02-10 23:51:45 +0900

    feat: 필터,정렬 조건 API 작성 (#37)
    Signed-off-by: 이승호 <dltmdgh579@gmail.com>

--- src/main/java/ministryofeducation/sideprojectspring/personnel/presentation/PersonnelController.java
+++ src/main/java/ministryofeducation/sideprojectspring/personnel/presentation/PersonnelController.java
@@ -1,10 +1,14 @@
 package ministryofeducation.sideprojectspring.personnel.presentation;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import ministryofeducation.sideprojectspring.personnel.application.PersonnelService;
-import ministryofeducation.sideprojectspring.personnel.presentation.dto.request.PersonnelCondRequest;
+import ministryofeducation.sideprojectspring.personnel.domain.Gender;
+import ministryofeducation.sideprojectspring.personnel.domain.department_type.DepartmentType;
+import ministryofeducation.sideprojectspring.personnel.presentation.dto.request.PersonnelFilterCondRequest;
+import ministryofeducation.sideprojectspring.personnel.presentation.dto.request.PersonnelOrderCondRequest;
 import ministryofeducation.sideprojectspring.personnel.presentation.dto.request.PersonnelPostRequest;
 import ministryofeducation.sideprojectspring.personnel.presentation.dto.response.PersonnelDetailResponse;
 import ministryofeducation.sideprojectspring.personnel.presentation.dto.response.PersonnelListResponse;
@@ -13,7 +17,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
@@ -28,9 +31,17 @@ public class PersonnelController {
     private final PersonnelService personnelService;
 
     @GetMapping("/list")
-    public ResponseEntity<List<PersonnelListResponse>> personnelList(PersonnelCondRequest condition){
-        System.out.println("condition = " + condition.getDepartmentType1());
-        List<PersonnelListResponse> responseDto = personnelService.personnelList(condition);
+    public ResponseEntity<List<PersonnelListResponse>> personnelList(
+        @RequestParam(value = "departmentFilter", required = false) List<DepartmentType> departmentFilter,
+        @RequestParam(value = "genderFilter", required = false) Gender genderFilter,
+        @RequestParam(value = "order", required = false) PersonnelOrderCondRequest orderCond
+        ){
+
+        if(departmentFilter == null)
+            departmentFilter = new ArrayList<>();
+
+        List<PersonnelListResponse> responseDto = personnelService.personnelList(
+            new PersonnelFilterCondRequest(departmentFilter, genderFilter), orderCond);
 
         return ResponseEntity.ok(responseDto);
     }

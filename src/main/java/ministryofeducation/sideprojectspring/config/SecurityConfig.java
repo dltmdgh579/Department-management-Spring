9e53f3592611099dc0d50114c74d9cfea0171f1b
이승호 <dltmdgh579@gmail.com>
2024-06-20 23:24:17 +0900

    add: Security allowed origin add - dnch-edu
    Signed-off-by: 이승호 <dltmdgh579@gmail.com>

--- src/main/java/ministryofeducation/sideprojectspring/config/SecurityConfig.java
+++ src/main/java/ministryofeducation/sideprojectspring/config/SecurityConfig.java
@@ -88,8 +88,8 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource(){
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowCredentials(true);
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
-        configuration.setAllowedMethods(Arrays.asList("GET","POST"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://dnch-edu.com"));
+        configuration.setAllowedMethods(Arrays.asList("*"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
